06fa0c2f8735071336b511ec42dc6bdaaf385eb2
kodinkat <me@karlkatewu.com>
2021-10-14 16:55:33 +0100

    UTF8 decoding post transient usage

--- includes/admin/admin-menu-and-tabs.php
+++ includes/admin/admin-menu-and-tabs.php
@@ -221,3 +221,15 @@ function disciple_tools_import_sanitize_array( &$array ) {
     }
     return $array;
 }
+
+function disciple_tools_import_array_utf8_decode( &$array ) {
+    foreach ( $array as &$value ) {
+        if ( ! is_array( $value ) ) {
+            $value = utf8_decode( $value );
+        } else {
+            disciple_tools_import_array_utf8_decode( $value );
+        }
+    }
+
+    return $array;
+}
